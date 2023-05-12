dc36511e8bc8356906a3d0deb01a69f7b96e39c9
Rafl27 <raflsouza27@gmail.com>
2023-05-12 20:04:07 -0300

    medicine types

---
+++ enums/medicineTypeEnum.js
@@ -0,0 +1,10 @@
+const medicineTypeEnum = {
+    COMPRIMIDO: 'Comprimido',
+    MILILITROS: 'Liquido',
+    MILIGRAMA: 'Miligrama',
+    GRAMA: 'GRAMA',
+    SPRAY: 'SPRAY',
+    GOTA: 'GOTA',
+};
+
+module.exports = medicineTypeEnum;
